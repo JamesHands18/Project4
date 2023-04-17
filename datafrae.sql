b98fb01782ab3c48ba33940b56edbc0dd9feca9a
tharu heenatigala <tharushaheenatigala@gmail.com>
2023-04-17 19:58:02 +0100

    changes by tharu

---
+++ datafrae.sql
@@ -0,0 +1,13 @@
+CREATE TABLE dataframe (
+    carat FLOAT,
+	cut TEXT,
+	color TEXT,
+	clarity TEXT,
+	"depth" FLOAT,
+	"table" FLOAT,
+	price FLOAT,
+	"'x'" FLOAT,
+	"'y'" FLOAT,
+	"'z'" FLOAT
+);
+SELECT * FROM dataframe;
